a09d4cdf9051c1c79d0943da781e9648918aabeb
kitoko <alfonsomartinez.alu@iespacomolla.es>
2023-10-06 12:30:54 +0200

    06/10/2023

--- app/src/main/java/com/example/saludaalumno/MainActivity.kt
+++ app/src/main/java/com/example/saludaalumno/MainActivity.kt
@@ -3,25 +3,37 @@ package com.example.saludaalumno
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.saludaalumno.ui.theme.SaludaAlumnoTheme
 
 class MainActivity : ComponentActivity() {
+	private lateinit var saludo: TextView
+	private lateinit var introNombre: EditText
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContent {
-			setContentView(R.layout.actividad_principal)
-			fun pulsaBoton(view: View) {
-				val button: Button =findViewById(R.id.boton)
-			}
+		setContentView(R.layout.layout_principal)
+		/*val boton:Button=findViewById(R.id.boton)
+		saludar=findViewById(R.id.saludo)
+		boton.setOnClickListener {saludo()}
+		fun saludo(){
+			val mensaje="pepe"
+			boton.text=mensaje
 		}
+		/*boton?.setOnClickListener(){
+			string(this@MainActivity,R.string.saludo,R.string.introNombre).show()
+		}*/*/
+		val boton:Button=findViewById(R.id.boton)
+		saludo=findViewById(R.id.saludo)
+		introNombre=findViewById(R.id.nombre)
+		boton.setOnClickListener{saludar()}
+	}
+	private fun saludar(){
+		val nombre=introNombre.text.toString()
+		val saludos=getString(R.string.saludo)
+		val mensaje="$saludos $introNombre"
+		saludo.text=mensaje
 	}
 }
